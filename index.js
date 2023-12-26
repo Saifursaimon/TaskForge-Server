04bbafabe66b2b7e115097a10d317e1be3466500
Saimon <saimon32kg@gamil.com>
2023-12-26 18:08:41 +0600

    'middleware'

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 function verify(req, res, next) {
   const authHeader = req.headers.authorization;
